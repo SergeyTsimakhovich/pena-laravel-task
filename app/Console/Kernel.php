187e3cc37a36547f4c7f0fd6cc7efcda9ad1411e
Sergey Tsimakhovich <tmacprison@gmail.com>
2023-02-11 01:31:19 +0500

    save phpstorm

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-	    //gg
+	    //gg 123
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
